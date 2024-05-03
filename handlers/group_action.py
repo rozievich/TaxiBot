44964c987f79c70e49719f309d6379acb44fd501
Rozievich <blackcoder459@outlook.com>
2024-05-03 22:51:41 +0500

    update code by rozievich

--- handlers/group_action.py
+++ handlers/group_action.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, types
+from aiogram import Bot, types, F
 from data.config import FROM_GROUP_ID, TO_GROUP_ID, KEYBOARDS
 from .first_commans import mainrouter
 
@@ -10,10 +10,20 @@ async def forward_message_to_small_group(message: types.Message, bot: Bot):
 
 
 async def message_contains_keyword(message: str):
-    for keyword in KEYBOARDS:
-        if keyword in message:
-            return True
-    return False
+    if message:
+        for keyword in KEYBOARDS:
+            if keyword in message:
+                return True
+        return False
+    else:
+        return False
+
+
+@mainrouter.message(F.new_chat_member)
+async def member_joined(message: types.Message):
+    if message.chat.id == FROM_GROUP_ID and message.new_chat_members:
+        new_member = message.new_chat_members[0]
+        await message.reply(f"Assalomu alaykum {new_member.full_name} 🤖\nBizning bot orqali tez va oson taxi toping ✅\nShunchaki botga kiring va <a href='https://t.me/Namangan_Samarqand_Taxi_bot?start=true'>/start</a> tugmasini bosing 🎛")
 
 
 @mainrouter.message()
@@ -21,3 +31,4 @@ async def handle_big_group_messages(message: types.Message, bot: Bot):
     if message.chat.type == "supergroup" and message.chat.id == FROM_GROUP_ID:
         if await message_contains_keyword(message=message.text):
             await forward_message_to_small_group(message, bot)
+
