3f41d1a6d60ccf291eba1b8847c626f563ab18fb
Rozievich <blackcoder459@outlook.com>
2024-09-05 00:51:55 +0500

    Update data and clear code

--- keyboards/inline/user_button.py
+++ keyboards/inline/user_button.py
@@ -1,8 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from data.config import bot_link
+from data.config import BOT_LINK
 
 button = InlineKeyboardMarkup(
     inline_keyboard=[[
-        InlineKeyboardButton(text="➕", url=bot_link)
+        InlineKeyboardButton(text="➕", url=BOT_LINK)
     ]]
 )
